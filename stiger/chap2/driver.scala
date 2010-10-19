38651421d52dd3340bc1f0124931706323e36e54
h_sakurai <sakurai777@gmail.com>
2010-10-20 03:31:43 +0900

    add chap2

--- stiger/chap2/driver.scala
+++ stiger/chap2/driver.scala
@@ -1,6 +1,6 @@
 object Parse {
 	def parse(filename:String) {
-		val file = TextIO.openIn(filename);
+		val file = new java.io.FileInputStream(filename)
 		def get(a) = TextIO.input(file, a)
 		val lexer = Mlex.makeLexer(get(_))
 		def do_it() {
@@ -9,6 +9,6 @@ object Parse {
 			if(t.substring(0,3)=="EOF") Unit else do_it()
 		}
 		do_it()
-		TextIO.closeIn(file)
+		file.close()
 	}
 }
